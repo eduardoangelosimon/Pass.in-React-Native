594f709a9e12d6123eb5293fd7dde70eb589ced3
Eduardo Angelo Simon <eduardosimon.trabalho@gmail.com>
2024-04-01 22:26:44 -0300

    Aula 1

---
+++ src/app/index.tsx
@@ -0,0 +1,28 @@
+import { View, Image, StatusBar } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Input } from "@/components/input";
+import { colors } from "@/styles/colors";
+import { Button } from "@/components/button";
+import { Link } from "expo-router";
+
+export default function Home() {
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar barStyle="light-content" />
+      <Image source={require("@/assets/logo.png")} className="h-16" resizeMode="contain" />
+
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <MaterialCommunityIcons name="ticket-confirmation-outline" color={colors.green[200]} size={20} />
+          <Input.Field placeholder="Código do Ingresso" />
+        </Input>
+
+        <Button title="Realizar inscrição" onPress={() => console.log("clicou!")} />
+
+        <Link href="/register" className="text-gray-100 text-base font-bold text-center mt-8">
+          Ainda não possui ingresso?
+        </Link>
+      </View>
+    </View>
+  );
+}
