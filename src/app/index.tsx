90c27669de04a4a209c7b902aaf161e8536687ac
Eduardo Simon <eduardosimon.trabalho@gmail.com>
2024-04-04 02:02:29 -0300

    Aula 3

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,41 @@
-import { View, Image, StatusBar } from "react-native";
+import { View, Image, StatusBar, Alert } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Input } from "@/components/input";
 import { colors } from "@/styles/colors";
 import { Button } from "@/components/button";
-import { Link } from "expo-router";
+import { Link, Redirect } from "expo-router";
+import { useState } from "react";
+
+import { api } from "@/server/api";
+import { useBadgeStore } from "@/store/badge-store";
 
 export default function Home() {
+  const [code, setCode] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const badgeStore = useBadgeStore();
+
+  async function handleAccessCredential() {
+    try {
+      if (!code.trim()) {
+        Alert.alert("Ingresso", "Informe o código do ingresso!");
+      }
+
+      setIsLoading(true);
+
+      const { data } = await api.get(`/attendees/${code}/badge`);
+      badgeStore.save(data.badge);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      Alert.alert("Ingresso", "incresso não encontrado!");
+    }
+  }
+
+  if (badgeStore.data?.checkInURL) {
+    return <Redirect href="/ticket" />;
+  }
+
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content" />
@@ -14,10 +44,10 @@ export default function Home() {
       <View className="w-full mt-12 gap-3">
         <Input>
           <MaterialCommunityIcons name="ticket-confirmation-outline" color={colors.green[200]} size={20} />
-          <Input.Field placeholder="Código do Ingresso" />
+          <Input.Field placeholder="Código do Ingresso" onChangeText={setCode} />
         </Input>
 
-        <Button title="Realizar inscrição" onPress={() => console.log("clicou!")} />
+        <Button title="Acessar credencial" onPress={handleAccessCredential} isLoading={isLoading} />
 
         <Link href="/register" className="text-gray-100 text-base font-bold text-center mt-8">
           Ainda não possui ingresso?
--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,11 +1,61 @@
-import { View, Image, StatusBar } from "react-native";
+import { View, Image, StatusBar, Alert } from "react-native";
 import { FontAwesome6, MaterialIcons } from "@expo/vector-icons";
 import { Input } from "@/components/input";
 import { colors } from "@/styles/colors";
 import { Button } from "@/components/button";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { useState } from "react";
+import axios from "axios";
+import { useBadgeStore } from "@/store/badge-store";
+
+import { api } from "@/server/api";
+
+const EVENT_ID = "55827cba-ae49-4a52-9f42-eefafc17279b";
 
 export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const badgeStore = useBadgeStore();
+
+  async function handleRegister() {
+    try {
+      if (!name.trim() || !email.trim()) {
+        return Alert.alert("Iscrição", "Preencha todos os campos!");
+      }
+      setIsLoading(true);
+
+      const registerResponse = await api.post(`/events/${EVENT_ID}/attendees`, { name, email });
+
+      if (registerResponse.data.attendeeId) {
+        const badgeResponse = await api.get(`/attendees/${registerResponse.data.attendeeId}/badge`);
+
+        badgeStore.save(badgeResponse.data.badge);
+
+        Alert.alert("Iscrição", "Inscrição realizada com sucesso!", [
+          {
+            text: "OK",
+            onPress: () => router.push("/ticket"),
+          },
+          {
+            text: "Cancelar",
+          },
+        ]);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      if (axios.isAxiosError(error)) {
+        if (String(error.response?.data.message).includes("already registered")) {
+          return Alert.alert("Inscrição", "Este e-mail já está cadastrado!");
+        }
+      }
+
+      Alert.alert("Iscrição", "Não foi possível fazer a inscrição!");
+    }
+  }
+
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content" />
@@ -14,15 +64,15 @@ export default function Register() {
       <View className="w-full mt-12 gap-3">
         <Input>
           <FontAwesome6 name="user-circle" color={colors.green[200]} size={20} />
-          <Input.Field placeholder="Nome completo" />
+          <Input.Field placeholder="Nome completo" onChangeText={setName} />
         </Input>
 
         <Input>
           <MaterialIcons name="alternate-email" color={colors.green[200]} size={20} />
-          <Input.Field placeholder="E-mail" keyboardType="email-address" />
+          <Input.Field placeholder="E-mail" keyboardType="email-address" onChangeText={setEmail} />
         </Input>
 
-        <Button title="Acessar credencial" onPress={() => console.log("clicou!")} />
+        <Button title="Realizar inscrição" onPress={handleRegister} isLoading={isLoading} />
 
         <Link href="/" className="text-gray-100 text-base font-bold text-center mt-8">
           Já possui ingresso?
